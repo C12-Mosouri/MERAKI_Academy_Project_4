604dca4a361f54be5b2b214bbfd83ce7eb9746e1
Mosouri <mohommad.souri@gmail.com>
2024-10-10 22:23:33 +0300

    14 Frontend Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { createContext, useState } from "react";
 import "./App.css";
-const App = () => {
-  return (
-   <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  )
-}
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Login from "./components/shared components/Login";
+export const tokenContext = createContext();
 
-export default App
+const App = () => {
+  const [token, setToken] = useState("" || localStorage.getItem("token"));
+  return (
+    <>
+      <tokenContext.Provider value={{ token, setToken }}>
+        <div className="App">
+          <h1>Hello World!</h1>
+          <Login />
+        </div>
+      </tokenContext.Provider>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/shared components/Login.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import App from "../../App";
+import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+import { tokenContext } from "../../App";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [user, setUser] = useState("");
+  const { setToken } = useContext(tokenContext);
+  // const navigate = useNavigate();
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <br></br>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></input>
+      <br></br>
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      ></input>
+      <br />
+      <button
+        onClick={() => {
+          const userLogin = {
+            email,
+            password,
+          };
+          axios
+            .post("http://localhost:5000/users/login", userLogin)
+            .then((res) => {
+              console.log(res.data.message);
+              setUser(true);
+              console.log(user);
+              setMessage(res.data.message);
+              console.log(res.data.token);
+              setToken(res.data.token);
+              localStorage.setItem("token", res.data.token);
+              // navigate("/dashboard");
+            })
+            .catch((err) => {
+              console.log(err);
+              setUser(false);
+              setMessage(err.response.data.message);
+            });
+        }}
+      >
+        Login
+      </button>
+      {setUser ? <h1>{message}</h1> : <h1>{message}</h1>}
+    </div>
+  );
+};
+
+export default Login;

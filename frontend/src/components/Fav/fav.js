313ddd8effac434d41d84a5f849b0558773749be
Mosouri <mohommad.souri@gmail.com>
2024-10-16 17:37:17 +0300

    37 Fav

--- frontend/src/components/Fav/fav.js
+++ frontend/src/components/Fav/fav.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 const Fav = () => {
   const [fav, setFav] = useState([]);
-  const [favId, setFavId] = useState("");
+  const [favId, setFavId] = useState([]);
   const { token, userId } = useContext(tokenContext);
   // const { id } = useParams();
 

fbe9ba0a8bcf3347c901e333b7df28e9eb9a5079
Mosouri <mohommad.souri@gmail.com>
2024-10-18 18:26:29 +0300

    47 css

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-// import ExampleCarouselImage from "components/ExampleCarouselImage";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [img, setImg] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/product/`)
+      .then((result) => {
+        console.log(result.data.Category);
+        setImg(result.data.Category);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },[]);
   return (
     <>
       <div>home</div>
@@ -44,9 +58,12 @@ const Home = () => {
         </Carousel.Item>
       </Carousel>
 
-      <div  className="category-container" onClick={()=>{
-        navigate("/category")
-      }}>
+      <div
+        className="category-container"
+        onClick={() => {
+          navigate("/category");
+        }}
+      >
         <div className="category-card">
           <img
             src="https://i.pinimg.com/736x/10/a3/6b/10a36b66475f91e4ca658f276ae6037f.jpg"
@@ -55,6 +72,49 @@ const Home = () => {
           <h3>Football</h3>
         </div>
       </div>
+      <CardGroup>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            {/* <Card.Title>Card title</Card.Title> */}
+            {/*  <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text> */}
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted"></small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </>
   );
 };
